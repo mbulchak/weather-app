7f6db762edfc05a000e0645fcb699f9393608ccf
Mariia Bulchak <bulchakmariiast@gmail.com>
2025-05-12 15:54:28 +0300

    fix: fix base name for Versel deploy

--- vite.config.ts
+++ vite.config.ts
@@ -5,7 +5,7 @@ import tailwindcss from '@tailwindcss/vite'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react(), tailwindcss()],
-  base: '/weather-app/',
+  base: '/',
   test: {
     environment: 'happy-dom',
     setupFiles: ['./src/tests/setup.ts'],
